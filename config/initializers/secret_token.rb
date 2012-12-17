6af267360757a2f91b5fce6377510ec083ff54fa
user <user@test.ru>
2012-12-17 18:54:37 +0400

    create my project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PasswordSite::Application.config.secret_token = 'e440547126fbc1d9126489a4fac0ec36792fd15fafdf72d485c6c02f34e94777c736233e61628f7cb4cc8a3118b33a28e143e7b98ba0855d749a3e8e9f6ee85b'
